1297db614e357972f04b9889f9a07e04fa47b1bb
Andy Christianson <nix@nixxed.com>
2019-11-03 21:16:57 -0600

    Updates

---
+++ precache-manifest.fa67729f08031627ee26b70406d2d2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7397b24d62c6d0f97eef9660bf59553f",
+    "url": "/bghelper/index.html"
+  },
+  {
+    "revision": "e73b0d0ea970b8bb52ef",
+    "url": "/bghelper/static/css/main.5e56a34f.chunk.css"
+  },
+  {
+    "revision": "e7e795ff7d6bf35c1f0a",
+    "url": "/bghelper/static/js/2.b92ea57d.chunk.js"
+  },
+  {
+    "revision": "e73b0d0ea970b8bb52ef",
+    "url": "/bghelper/static/js/main.cdca23b2.chunk.js"
+  },
+  {
+    "revision": "7f0018c57b87a7ad67bd",
+    "url": "/bghelper/static/js/runtime-main.8fe7d613.js"
+  },
+  {
+    "revision": "7337e417b1d92c64da0bf9bfe18fe85a",
+    "url": "/bghelper/static/media/github.7337e417.svg"
+  }
+]);
