3c43842ff9de1be72e7c19d3af098fa45ce5f470
Andy Christianson <nix@nixxed.com>
2019-11-04 19:56:29 -0600

    fix: Card max width should be correct.

--- src/shared/Card.js
+++ src/shared/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MuiCard from '@material-ui/core/Card'
 
-function Card({children, width = 300, maxWidth = 300}) {
+function Card({children, width = 300, maxWidth = width}) {
     const style = {width, maxWidth, margin: 16}
     return <MuiCard style={style}>{children}</MuiCard>
 }
