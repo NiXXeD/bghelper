960381b3534d9d82d333d100658afdea13c9e213
Andy Christianson <nix@nixxed.com>
2020-01-21 11:06:04 -0600

    fix: Remove unimplemented games.

--- src/games/games.js
+++ src/games/games.js
@@ -1,13 +1,7 @@
 import Barenpark from './barenpark/Barenpark'
 import FoodChainMagnate from './foodChainMagnate/FoodChainMagnate'
-import QuacksOfQuedlinberg from './quacksOfQuedlinberg/QuacksOfQuedlinberg'
-import RollPlayer from './rollPlayer/RollPlayer'
-import Unearth from './unearth/Unearth'
 
 export default [
     {name: 'Bärenpark', path: '/barenpark', component: Barenpark},
-    {name: 'Food Chain Magnate', path: '/foodChainMagnate', component: FoodChainMagnate},
-    {name: 'Quacks of Quedlinberg', path: '/quacksOfQuedlinberg', component: QuacksOfQuedlinberg},
-    {name: 'Roll Player', path: '/rollPlayer', component: RollPlayer},
-    {name: 'Unearth', path: '/unearth', component: Unearth}
+    {name: 'Food Chain Magnate', path: '/foodChainMagnate', component: FoodChainMagnate}
 ]
